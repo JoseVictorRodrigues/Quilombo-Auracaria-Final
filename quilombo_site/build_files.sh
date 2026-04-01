263b5267bcb4d0529a8e3da119361d3ae125d894
Jose Victor Rodrigues Guimaraes <102203391+JoseVictorRodrigues@users.noreply.github.com>
2026-04-01 11:59:09 -0300

    Initial commit

--- quilombo_site/build_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script de build para Vercel
-# Instala dependências e coleta arquivos estáticos
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
